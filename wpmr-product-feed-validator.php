87377a76bd844ef182e53ebb94b0ffda12f8ed66
Auke Jongbloed <auke@wpmarketingrobot.com>
2025-11-27 09:22:01 +0100

    chore: bump version to 0.3.1
    Update plugin version and stable tag to 0.3.1 for release.
    
    Changes:
    - Update Version header to 0.3.1 in main plugin file
    - Update WPMR_PFV_VERSION constant to 0.3.1
    - Update Stable tag to 0.3.1 in README.txt
    
    Ready for v0.3.1 release with feed quality enhancements.

--- wpmr-product-feed-validator.php
+++ wpmr-product-feed-validator.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       WPMR Product Feed Validator
  * Description:       Validate Google Shopping product feeds and email/share reports.
- * Version:           0.3.0
+ * Version:           0.3.1
  * Author:            WP Marketing Robot
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
 
 // Constants - must be defined before any other code
 if (!defined('WPMR_PFV_VERSION')) {
-    define('WPMR_PFV_VERSION', '0.3.0');
+    define('WPMR_PFV_VERSION', '0.3.1');
 }
 if (!defined('WPMR_PFV_PLUGIN_FILE')) {
     define('WPMR_PFV_PLUGIN_FILE', __FILE__);
